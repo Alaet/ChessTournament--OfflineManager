777b7e2a78c38e43cd7176ecac82aa9da350fc6f
Alaet <72195619+Alaet@users.noreply.github.com>
2021-12-28 16:36:26 +0100

    Update Match
    add variable for evaluation state of match and change to match controller to generate match based on document from tinyDB

--- Match/controller.py
+++ Match/controller.py
@@ -1,9 +1,10 @@
 from .model import Match
-from Tournament.controller import TournamentController
+
 
 class MatchController:
 
-    def generate_match_list(self, sorted_list, versus):
+    @staticmethod
+    def generate_match_list(sorted_list, versus):
         """
         Take sorted dict and versus as ID to create tuple of match with object(Player) and player.score
         :param sorted_list: dict{"player": object(Player),
@@ -17,21 +18,17 @@ class MatchController:
                         [(Player, player.score), (Player, player.score)]
                       ]
         """
-        serealized_players = TournamentController.serialize_tournament_players(TournamentController, sorted_list)
         round_pair_player_list = []
-        serialized_round_pair_player_list = []
-        for match in versus:
-            opponents = [(serealized_players[match[0]], sorted_list[match[0]].score),
-                         (serealized_players[match[1]], sorted_list[match[1]].score)]
-            current_match = Match(opponents)
-            serialized_current_match = self.serialize_match(current_match)
-            serialized_round_pair_player_list.append(serialized_current_match)
-            round_pair_player_list.append(current_match)
-        return serialized_round_pair_player_list
 
-    @staticmethod
-    def serialize_match(match):
-        serialized_match = {
-            'match_info': match.match_info
-        }
-        return serialized_match
+        for match in versus:
+            try:
+                opponents = [(sorted_list[match[0]], sorted_list[match[0]].score),
+                             (sorted_list[match[1]], sorted_list[match[1]].score)]
+            except AttributeError:
+                opponents = [(sorted_list[match[0]], sorted_list[match[0]]['score']),
+                             (sorted_list[match[1]], sorted_list[match[1]]['score'])]
+            evaluated = False
+            current_match = Match(opponents, evaluated)
+
+            round_pair_player_list.append(current_match)
+        return round_pair_player_list
